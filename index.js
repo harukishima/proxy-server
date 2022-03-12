477307fb00ec52570f046bb8869a75fd5902a112
HaruKishima <18120256@student.hcmus.edu.vn>
2022-03-12 16:25:29 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+require("dotenv").config();
+const config = require("./config");
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = config.port;
+const HOST = "localhost";
+const API_SERVICE_URL = config.baseURL;
+
+app.use(morgan("dev"));
+
+// Info GET endpoint
+app.get("/info", (req, res, next) => {
+  res.send(
+    "This is a proxy service which proxies to Billing and Account APIs."
+  );
+});
+
+app.use(
+  "/proxy",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/proxy`]: "",
+    },
+  })
+);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
